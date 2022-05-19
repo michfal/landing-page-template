dec5427fb4557f2a81db7e0fd9027a96c7f0704e
Michał Fal <michfal86@gmail.com>
2022-05-19 17:37:46 +0200

    Hero animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { scrollPage } from './hero';
 import { handleSliderButton } from './slider.js';
 import { handleScrollAnimation } from './scroll';
+import { handleOnloadAnimation } from './hero';
 import '../scss/app.scss';
 
 import './slider.js';
@@ -11,3 +12,4 @@ const heroButton = document.querySelector('.j-hero_button');
 sliderButtons.addEventListener('click', handleSliderButton);
 heroButton.addEventListener('click', scrollPage);
 window.addEventListener('scroll', handleScrollAnimation);
+window.addEventListener('load', handleOnloadAnimation);
--- src/js/hero.js
+++ src/js/hero.js
@@ -1,5 +1,28 @@
+import { gsap } from 'gsap';
+
 const windowHeight = window.innerHeight;
+const animateElements = document.querySelectorAll('.j-animate_onload');
+const hero = document.querySelector('.hero');
+hero.style.opacity = 0;
 
 export function scrollPage() {
   window.scrollTo({ top: windowHeight, behavior: 'smooth' });
+}
+
+function animateHeaders(element) {
+  let startPosition = -200;
+
+  if (element.classList.contains('j-right')) {
+    startPosition = 200;
+  }
+
+  gsap.from(element, { x: startPosition, duration: 1 });
+}
+
+export function handleOnloadAnimation() {
+  gsap.to(hero, { opacity: 1, duration: 2 });
+  gsap.from('.j-bottom', { y: 100, duration: 1 });
+  animateElements.forEach((element) => {
+    animateHeaders(element);
+  });
 }
